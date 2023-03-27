0bf5625099a7ea05ef0771237ea6c44dc9dbf7c3
Alex H <alh311@gmail.com>
2023-03-27 13:48:11 -0600

    Building a menu using List * Adds a list with the menu sections and items

---
+++ iDine/iDine/Views/ContentView.swift
@@ -0,0 +1,35 @@
+//
+//  ContentView.swift
+//  iDine
+//
+//  Created by Alex Hutchins on 3/27/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    let menu = Bundle.main.decode([MenuSection].self, from: "menu.json")
+    
+    var body: some View {
+        NavigationStack {
+            List {
+                ForEach(menu) { section in
+                    Section(section.name) {
+                        ForEach(section.items) { item in
+                            Text(item.name)
+                        }
+                    }
+                }
+            }
+            .listStyle(.grouped)
+            .navigationTitle("Menu")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
