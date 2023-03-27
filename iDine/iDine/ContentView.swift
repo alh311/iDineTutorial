c5509700052f4493e4c6989822306cd64f59d70a
Alex H <alh311@gmail.com>
2023-03-27 12:04:09 -0600

    Project Creation * Creates new Xcode project for the app * Adds resources used by the tutorial

---
+++ iDine/iDine/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  iDine
+//
+//  Created by Alex Hutchins on 3/27/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iDine/iDine/iDineApp.swift
@@ -0,0 +1,17 @@
+//
+//  iDineApp.swift
+//  iDine
+//
+//  Created by Alex Hutchins on 3/27/23.
+//
+
+import SwiftUI
+
+@main
+struct iDineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
