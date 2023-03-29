e61bec92ae98cc8fdd26be556cc6f6630eec3b6a
Alex H <alh311@gmail.com>
2023-03-29 12:48:05 -0600

    Bindings and forms * Adds checkout screen with payment type picker

---
+++ iDine/iDine/Views/CheckoutView.swift
@@ -0,0 +1,38 @@
+//
+//  CheckoutView.swift
+//  iDine
+//
+//  Created by Alex Hutchins on 3/29/23.
+//
+
+import SwiftUI
+
+struct CheckoutView: View {
+    @EnvironmentObject var order: Order
+    @State private var paymentType = "Cash"
+    
+    let paymentTypes = ["Cash", " Credit Card", "iDine Points"]
+    
+    var body: some View {
+        VStack {
+            Section {
+                Picker("How do you want to pay?", selection: $paymentType) {
+                    ForEach(paymentTypes, id: \.self) {
+                        Text($0)
+                    }
+                }
+            }
+        }
+        .navigationTitle("Payment")
+        .navigationBarTitleDisplayMode(.inline)
+    }
+}
+
+struct CheckoutView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationStack {
+            CheckoutView()
+                .environmentObject(Order())
+        }
+    }
+}
